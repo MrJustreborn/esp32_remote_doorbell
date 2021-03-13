36022b537bca05b4f44be8efe42156b487a27e8c
René Thomas Lottes <rene.lottes@gmx.de>
2021-03-13 10:12:01 +0100

    Dockerrize app

--- server/app.py
+++ server/app.py
@@ -18,7 +18,7 @@ localhost_key = pathlib.Path(__file__).with_name("key.pem")
 ssl_context.load_cert_chain(localhost_pem, keyfile=localhost_key)
 
 start_server = websockets.serve(
-    hello, "localhost", 8765, ssl=ssl_context
+    hello, "*", 8765, ssl=ssl_context
 )
 
 asyncio.get_event_loop().run_until_complete(start_server)
