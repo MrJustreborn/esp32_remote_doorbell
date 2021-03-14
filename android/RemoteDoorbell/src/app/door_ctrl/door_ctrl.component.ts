8df75d69d96e1c3e17a92e161ea8c13af8f7d1da
René Thomas Lottes <rene.lottes@gmx.de>
2021-03-14 09:00:09 +0100

    Added font awesome and edited layout

--- android/RemoteDoorbell/src/app/door_ctrl/door_ctrl.component.ts
+++ android/RemoteDoorbell/src/app/door_ctrl/door_ctrl.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from "@angular/core";
-import { Button, EventData, Switch } from "@nativescript/core";
+import { Button, EventData, Slider, Switch } from "@nativescript/core";
 import { firebase } from "@nativescript/firebase";
 
 
@@ -9,17 +9,51 @@ import { firebase } from "@nativescript/firebase";
 })
 export class DoorCtrlComponent implements OnInit {
 
+    columns;
+    visitorModeActive = false;
+
+    visitorModeTimme = 10;
+
     constructor() { }
 
     ngOnInit(): void {
+        this.setProgressbarWidth(100);
+    }
+
+    setProgressbarWidth(percent) {
+        this.columns = percent + "*," + (100 - percent) + "*";
+    }
+
+
+    onSliderValueChange(args: EventData) {
+        let slider = args.object as Slider;
+        console.log("onSliderValueChange", slider.value)
+        this.visitorModeTimme = slider.value;
+    }
+
+    activateVisitorsMode(args: EventData) {
+        let button = args.object as Button;
+        console.log("activateVisitorsMode")
+        this.visitorModeActive = true;
+    }
+
+    deactivateVisitorsMode(args: EventData) {
+        let button = args.object as Button;
+        console.log("deactivateVisitorsMode")
+        this.visitorModeActive = false;
+        this.visitorModeTimme = 10;
+    }
+
+    onProgressLoaded() {
+        this.visitorModeActive = false;
     }
 
     onOpenDoor(args: EventData) {
         let button = args.object as Button;
         console.log("Open door!")
-        firebase.getCurrentPushToken().then((token: string) => {
-            // may be null if not known yet
-            console.log(`Current push token: ${token}`);
-          });
+    }
+
+    gotToSettings() {
+        console.log("GotToSettings")
     }
 }
